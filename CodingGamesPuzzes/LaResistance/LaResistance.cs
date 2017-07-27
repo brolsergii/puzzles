6014be1c2d7bb5177a3e5012bf72ba19680e07d2
Sergii BROLINSKYI <sergii.brolinskyi@mediamobile.com>
2017-07-27 15:38:22 +0200

    Adds La resistance start code

---
+++ CodingGamesPuzzes/LaResistance/LaResistance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Solution
+{
+    #region Auxilary methods
+    static void TryCatch(Action a) { try { a(); } catch (Exception) { } }
+    static int TryGetInt(Func<int> a) { try { return a(); } catch (Exception) { return 0; } }
+    static void Deb(object o) => Console.Error.WriteLine(o);
+    static void DebList(IEnumerable<object> e) => Console.Error.WriteLine(e.Aggregate((x, y) => $"{x} {y}"));
+    static void DebObjList(IEnumerable<object> e) => TryCatch(() => Console.Error.WriteLine(e.Aggregate((x, y) => $"{x}\n{y}")));
+    static void DebDict(Dictionary<int, int> d)
+    {
+        foreach (var pair in d)
+            Console.Error.WriteLine($"[{pair.Key}]:{pair.Value}");
+    }
+    #endregion
+
+    #region Global context
+    static HashSet<string> vocabulary = new HashSet<string>();
+    #endregion
+
+    static void Main(string[] args)
+    {
+        string morseLength = Console.ReadLine();
+        int dictSize = int.Parse(Console.ReadLine());
+        for (int i = 0; i < dictSize; i++)
+            vocabulary.Add(Console.ReadLine());
+
+        Console.WriteLine("answer");
+    }
+}
